8e4756a7b58dd2af547cbfa56e6b47271003611e
Nikolaus Leopold <nikolaus.leopold@gmail.com>
2017-03-25 12:24:16 +0100

    task1 and CMake build files

---
+++ task1/src/main.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main()
+{
+
+    Mat img1, img2;
+    img1 = imread("images/tsukuba_left.png", CV_LOAD_IMAGE_COLOR);
+    img2 = imread("images/tsukuba_right.png", CV_LOAD_IMAGE_COLOR);
+    if (!img1.data || !img2.data) {
+        std::cout <<  "INPUT ERROR: Could not open or find images" << std::endl;
+        return -1;
+    }
+
+    Mat imgAbsDiff;
+    absdiff(img1, img2, imgAbsDiff); // element-wise absolute difference
+
+    namedWindow("OpenCV", WINDOW_AUTOSIZE);
+    imshow("OpenCV", imgAbsDiff);
+
+    waitKey(0); // wait for a keystroke in the window
+    
+    return 0;
+    
+}
+
